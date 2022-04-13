2555128f786ac085e53be78fb0ed26e30d8ee2f4
lazaronazareno <lazaronazareno@gmail.com>
2022-04-13 03:16:18 -0300

    middle of csvtojson, file upload but fail conversion

--- movie-server/src/routes/movies.ts
+++ movie-server/src/routes/movies.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as moviesServices from '../services/moviesServices'
+import csv from 'csvtojson'
 
 import toNewMovie from '../utils'
 import { fileUpload } from '../middleware/uploadfile'
@@ -30,7 +31,45 @@ router.post('/', (req, res) => {
 })
 
 router.post('/post/data', fileUpload, (req: express.Request, res: express.Response) => {
-  console.log(req.file)
+  req.getConnection?.(async (err: any, conn: any) => {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (err) res.status(500).send('server error')
+    console.log(req.file?.filename)
+    const csvFileName = req.file?.filename
+
+    await csv().fromFile(csvFileName as string).then((source) => {
+      for (let i = 0; i < source.length; i++) {
+        const Titulo = source[i].Titulo
+        const Genero = source[i].Genero
+        const Año = source[i]['Año']
+        const Director = source[i].Director
+        const Actores = source[i].Actores
+
+        const insertStatement = 'INSERT INTO movies(?, ?, ?, ?, ?)'
+        const items = [Titulo, Genero, Año, Director, Actores]
+
+        conn.query(insertStatement, items, (err: any, _results: any, _fields: any) => {
+          // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+          if (err) {
+            console.log('Unable to insert item at row', i + 1)
+            return console.log(err)
+          }
+        })
+      }
+      console.log('Data stored in movies database')
+    }, function (err) {
+      console.log(err)
+    })
+
+    conn.query('DROP TABLE sample', (_err: any, _drop: any) => {
+      const createStatament =
+          'create table movielist( titulo varchar(255) not null unique, genero varchar(255) not null, año char(4) not null, director varchar(255) not null, actores varchar(255) not null, primary key(titulo))'
+      conn.query(createStatament, (_err: any, _drop: any) => {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        if (err) return console.log(err)
+      })
+    })
+  })
 })
 
 export default router
